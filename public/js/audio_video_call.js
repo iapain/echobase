ea08715f81dba6f48d66240177d03be5ce87b060
michal <michal@michal-VirtualBox.(none)>
2013-11-09 16:54:52 +0100

    implement AudioVideoChat as a singleton

--- public/js/audio_video_call.js
+++ public/js/audio_video_call.js
@@ -1,77 +1,105 @@
-// one to one call
-if(!window.EchoBase) window.EchoBase = {};
+// Audio/Video chat
+// This should be a singleton class as the user can be involved in one audio/video conversation at a time
+// it may be either one-to-one call or multiuser conference
+// AudioVideoChat.get(roomId, peer);
+
+if(!window.EchoBase)
+  window.EchoBase = {};
 window.EchoBase.roomId = "connection-room-id";
 
-var AudioVideoCall;
+// Compatibility shim
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-AudioVideoCall = (function() {
+var AudioVideoChat;
 
-  function AudioVideoCall(roomId, peer) {
-    this.roomId = roomId;
-    this.peer = peer;
-    this.existingCalls = [];
-    this.$myVideoDiv = $('#my-video');
-    this.startLocalStream().peerActions();
-  }
+AudioVideoChat = (function() {
+  var instance, _AudioVideo;
 
-  // start local video stream
-  AudioVideoCall.prototype.startLocalStream = function() {
-    var _this = this;
-    navigator.getUserMedia({audio: true, video: true}, function(stream){
-      // Set your video displays
-      _this.$myVideoDiv.prop('src', URL.createObjectURL(stream));
+  function AudioVideoChat(roomId, peer) {}
 
-      window.localStream = stream;
-    }, function(){ /* show error */ });
-    return this;
-  };
+  instance = null;
 
-  AudioVideoCall.prototype.makeCall = function() {
-    // take info from room about participants and try to connect them
-    this.participants = [];
-    for(var participant in this.participants) {
-      var call = peer.call(participant, window.localStream);
-      this.establishConnection(call);
+  _AudioVideo = (function() {
+
+    function _AudioVideo(roomId, peer) {
+      this.roomId = roomId;
+      this.peer = peer;
+      this.existingCalls = [];
+      this.$myVideoDiv = $('#my-video');
+      this.startLocalStream().peerActions();
     }
-  };
 
-  AudioVideoCall.prototype.endCall = function() {
-    for(var call in this.existingCalls) {
-      call.close();
+    // start local video stream
+    _AudioVideo.prototype.startLocalStream = function() {
+      var _this = this;
+      navigator.getUserMedia({audio: true, video: true}, function(stream){
+        // Set your video displays
+        _this.$myVideoDiv.prop('src', URL.createObjectURL(stream));
+
+        window.localStream = stream;
+      }, function(){ /* show error */ });
+      return this;
+    };
+
+    _AudioVideo.prototype.makeCall = function() {
+      // take info from room about participants and try to connect them
+      this.participants = [];
+      for(var participant in this.participants) {
+        var call = peer.call(participant, window.localStream);
+        this.establishConnection(call);
+      }
+    };
+
+    _AudioVideo.prototype.endCall = function() {
+      for(var call in this.existingCalls) {
+        call.close();
+      }
     }
-  }
 
-  AudioVideoCall.prototype.establishConnection = function(call) {
-    // save in a db an info that user is in a call
-    // Wait for stream on the call, then set peer video display
-    call.on('stream', function(stream){
-      var $video = $('#their-video-' + call.peer);
-      if($video[0])
-        $video.prop('src', URL.createObjectURL(stream));
-      else
-        $('#video-container').append($('<video/>').prop('id', call.peer).prop('autoplay', true).prop('src', URL.createObjectURL(stream)));
-    });
+    _AudioVideo.prototype.establishConnection = function(call) {
+      // save in a db an info that user is in a call
+      // Wait for stream on the call, then set peer video display
+      call.on('stream', function(stream){
+        var $video = $('#their-video-' + call.peer);
+        if($video[0])
+          $video.prop('src', URL.createObjectURL(stream));
+        else
+          $('#video-container').append($('<video/>').prop('id', call.peer).prop('autoplay', true).prop('src', URL.createObjectURL(stream)));
+      });
 
-    window.existingCalls.push(call);
+      window.existingCalls.push(call);
 
-    call.on('close', function() {
-      // do something with that
-    });
-    return this;
-  };
+      call.on('close', function() {
+        // do something with that
+      });
+      return this;
+    };
 
-  AudioVideoCall.prototype.peerActions = function() {
-    this.peer.on('call', function(call){
-      var caller = peer.id;
-      // var conf = confirm("" + caller + " is calling you.");
-      // if(conf == true){
-      call.answer(window.localStream);
-      this.establishConnection(call);
-      // }
-    });
-  }
+    _AudioVideo.prototype.peerActions = function() {
+      this.peer.on('call', function(call){
+        var caller = peer.id;
+        // var conf = confirm("" + caller + " is calling you.");
+        // if(conf == true){
+        call.answer(window.localStream);
+        this.establishConnection(call);
+        // }
+      });
+    };
 
-  return AudioVideoCall;
+    AudioVideoChat.get = function(roomId, peer) {
+      return instance != null ? instance : instance = new _AudioVideo(roomId, peer);
+    };
 
-})();
+    return _AudioVideo;
 
+  })();
+
+  return AudioVideoChat;
+
+}).call(this);
+
+// var peer = new Peer("mmmmmasasa", { key: 'lwjd5qra8257b9', debug: 1, config: {'iceServers': [
+//       { url: 'stun:stun.l.google.com:19302' } // Pass in optional STUN and TURN server for maximum network compatibility
+//     ]}});
+
+// AudioVideoChat.get('asdeded', peer);
